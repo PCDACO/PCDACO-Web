23417dc865204e567580b19fd8fc3c5c1a57dc83
Trung Anh <anhthtservice@gmail.com>
2025-02-05 23:56:23 +0700

    feat: implement firebase

--- components/amenities/columns.tsx
+++ components/amenities/columns.tsx
@@ -3,91 +3,99 @@
 import { MoreHorizontal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
 import { GetAmenitiesResponse } from "@/domains/models/amenities/getamenities.response";
 import { useState } from "react";
 import { AmenityDeleteDialog } from "@/components/amenities/delete-dialog";
 import { AmenityUpdateDialog } from "@/components/amenities/update-dialog";
-import { useDeleteAmenityRequest, useUpdateAmenityRequest } from "@/domains/stores/store";
-
+import {
+  useDeleteAmenityRequest,
+  useUpdateAmenityRequest,
+} from "@/domains/stores/store";
 
 export const columns: ColumnDef<GetAmenitiesResponse>[] = [
-    {
-        accessorKey: "id",
-        header: "ID",
-    },
-    {
-        accessorKey: "name",
-        header: "Name",
-    },
-    {
-        accessorKey: "description",
-        header: "Description",
-    },
-    {
-        accessorKey: "createdAt",
-        header: "Created At",
-    },
-    {
-        id: "actions",
-        cell: ({ row }) => {
-            // eslint-disable-next-line react-hooks/rules-of-hooks
-            const [IsDeleteOpen, SetIsDeleteOpen] = useState(false);
-            // eslint-disable-next-line react-hooks/rules-of-hooks
-            const [IsUpdateOpen, SetIsUpdateOpen] = useState(false);
-            // eslint-disable-next-line react-hooks/rules-of-hooks
-            const { setId, setDescription, setName } = useUpdateAmenityRequest();
-            // eslint-disable-next-line react-hooks/rules-of-hooks
-            const deleteRequest = useDeleteAmenityRequest();
-            const currentid = row.getValue("id") as string;
-            const currentname = row.getValue("name") as string;
-            const currentdescription = row.getValue("description") as string;
-            const openDeleteDialog = () => SetIsDeleteOpen(true);  // Open dialog
-            const closeDeleteDialog = () => SetIsDeleteOpen(false);  // Close dialog
-            const openUpdateDialog = () => SetIsUpdateOpen(true);
-            const closeUpdateDialog = () => SetIsUpdateOpen(false);
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const [IsDeleteOpen, SetIsDeleteOpen] = useState(false);
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const [IsUpdateOpen, SetIsUpdateOpen] = useState(false);
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const { setId, setDescription, setName } = useUpdateAmenityRequest();
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const deleteRequest = useDeleteAmenityRequest();
+      const currentid = row.getValue("id") as string;
+      const currentname = row.getValue("name") as string;
+      const currentdescription = row.getValue("description") as string;
+      const openDeleteDialog = () => SetIsDeleteOpen(true); // Open dialog
+      const closeDeleteDialog = () => SetIsDeleteOpen(false); // Close dialog
+      const openUpdateDialog = () => SetIsUpdateOpen(true);
+      const closeUpdateDialog = () => SetIsUpdateOpen(false);
 
-
-            return (
-                <><DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem onClick={() => {
-                            setId(currentid);
-                            setName(currentname);
-                            setDescription(currentdescription);
-                            openUpdateDialog();
-                        }} >
-                            <h2 >
-                                Update
-                            </h2>
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem onClick={() => {
-                            deleteRequest.setId(currentid);
-                            openDeleteDialog();
-                        }} >
-                            <h2 >
-                                Delete
-                            </h2>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu >
-                    <AmenityDeleteDialog isOpen={IsDeleteOpen} onClose={closeDeleteDialog} />
-                    <AmenityUpdateDialog isOpen={IsUpdateOpen} onClose={closeUpdateDialog} />
-                </>
-            );
-        },
+      return (
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem
+                onClick={() => {
+                  setId(currentid);
+                  setName(currentname);
+                  setDescription(currentdescription);
+                  openUpdateDialog();
+                }}
+              >
+                <h2>Update</h2>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={() => {
+                  deleteRequest.setId(currentid);
+                  openDeleteDialog();
+                }}
+              >
+                <h2>Delete</h2>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <AmenityDeleteDialog
+            isOpen={IsDeleteOpen}
+            onClose={closeDeleteDialog}
+          />
+          <AmenityUpdateDialog
+            isOpen={IsUpdateOpen}
+            onClose={closeUpdateDialog}
+          />
+        </>
+      );
     },
+  },
 ];
--- configs/firebase.config.ts
+++ configs/firebase.config.ts
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC5H_Wo1V1iMXUnIew5QQ-9B9I1l0fqvog",
-  authDomain: "pcdaco-61cb6.firebaseapp.com",
-  projectId: "pcdaco-61cb6",
-  storageBucket: "pcdaco-61cb6.firebasestorage.app",
-  messagingSenderId: "595190135675",
-  appId: "1:595190135675:web:d344cc62a30c956ec05bde",
-  measurementId: "G-TX1DTWWL1F"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
