fff6f17840c75294627a7ed70e1747b6d04d82ea
PersieDaCoder <thinhdpham2510@gmail.com>
2025-04-17 15:20:01 +0700

    update

--- components/cars/contract-dialog.tsx
+++ components/cars/contract-dialog.tsx
@@ -10,8 +10,8 @@ interface DocumentDialogProps {
 const CarContractDialog = ({ terms, open, onOpenChange }: DocumentDialogProps) => {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="min-w-[1000]" >
-        <div className="border rounded-md p-4 max-h-[850] overflow-auto">
+      <DialogContent className="max-h-[850] max-w-[1000]" >
+        <div className="border rounded-md p-4 max-h-[800] overflow-auto">
           <div
             dangerouslySetInnerHTML={{
               __html: terms,
